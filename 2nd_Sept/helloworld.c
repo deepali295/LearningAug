0bfcf4f09a4d1d07c7edbd3461d3206713c5255d
deepali295 <user25@trainux01.jrf11vejvqfuhbwnm0ewggipoa.ax.internal.cloudapp.net>
2024-09-02 11:02:56 +0000

    Updating

---
+++ 2nd_Sept/helloworld.c
@@ -0,0 +1,13 @@
+/*THis is a hello world program 
+Owner: Deepali Date:2/9/24
+Revision History
+1. Name: Deepali Date:3/9/24 Reason: 
+*/
+#include <stdio.h>
+int main()
+{
+	printf("My name is Deepali Kumari \n");
+	printf("My address is Padur,Chennai \n");
+	return 0;
+}
+
---
+++ commands.sh
@@ -0,0 +1,6 @@
+git add .
+sleep 2
+git commit -m "Updating"
+git push origin main
+echo files pushed to git
+
